2dd797b0b4443a605d688f7edf974bf3aa581da1
S-MW <72529306+S-MW@users.noreply.github.com>
2021-08-15 11:32:20 +0300

    add resorce contriller & add data by php artisan tinker

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Controllers\TodoListController::class ,'index'] );
+
+
+Route::resource('Todo', Controllers\TodoListController::class);
+
